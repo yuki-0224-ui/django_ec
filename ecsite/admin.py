0fa5709105c56196ab9b645321653bc413c4121a
yuki-0224-ui <sannomiya0224@gmail.com>
2024-12-17 14:19:51 +0900

    admin.pyにModelを追加

--- ecsite/admin.py
+++ ecsite/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from .models import Product, ProductImage
+from ecsite.models.product import Product, ProductImage
+from ecsite.models.order import Order, OrderItem
+from ecsite.models.cart import Cart, CartItem
 
 # Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
+
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Cart, CartAdmin)
